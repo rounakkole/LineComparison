9cd2c82088a34c879c081c206b976565a1348e20
rounak <rounakkole@gmail.com>
2022-02-14 13:22:41 +0530

    [rounak] ADDED: calculate length of a line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            double length=GetLength();
+            Console.WriteLine($"length: {length}");
+        }
+
+        public static double GetLength()
+        {
+            Console.WriteLine("eneter 4 values (x1, y1, x2, y2)");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double length = Math.Sqrt((Math.Pow((x2 - x1), 2)) + (Math.Pow((y2 - y1), 2)));
+            return length;
+        }
+
+
+
+    }
+}
